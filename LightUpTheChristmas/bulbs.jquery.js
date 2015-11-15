381eaa72344fed185e954cd67bfa9a4ead37f789
Sandros <me@sandros.hu>
2015-11-15 20:34:34 +0100

    Filtr. Codeparts edit

--- LightUpTheChristmas/bulbs.jquery.js
+++ LightUpTheChristmas/bulbs.jquery.js
@@ -1,5 +1,5 @@
 var christmasblock = 0;
-$("body").prepend('<div id="christmaslights" title="Te is szeretnél ilyet a blogodra / oldaladra? http://sandros.hu/?xmasbar"></div><audio id="christmaslightsaudio" src="https://github.com/sandrosdj/Garage/blob/master/LightUpTheChristmas/glass.ogg" preload></audio>');
+$("body").prepend('<div id="christmaslights" title="Te is szeretnÃ©l ilyet a blogodra / oldaladra? http://sandros.hu/?xmasbar"></div><audio id="christmaslightsaudio" src="https://github.com/sandrosdj/Garage/blob/master/LightUpTheChristmas/glass.ogg" type="audio/ogg" preload></audio>');
 for (var i = 0; i < (window.innerWidth / 64 - 1); i++)
 	$("#christmaslights").append("<div class='light color"+Math.floor(Math.random()*3)+"'></div>");
 var lutca = document.getElementById('christmaslightsaudio');
