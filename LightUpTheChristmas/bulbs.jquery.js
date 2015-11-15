b14d77e33c5ef793257572c24be0c63eb0db8cc0
Sandros <me@sandros.hu>
2015-11-15 20:17:04 +0100

    OTYOE Updata

--- LightUpTheChristmas/bulbs.jquery.js
+++ LightUpTheChristmas/bulbs.jquery.js
@@ -1,21 +1,19 @@
 var christmasblock = 0;
-$(document).ready(function() {
-	$("head").append($('<link rel="stylesheet" type="text/css" />').attr('href', 'https://raw.githubusercontent.com/sandrosdj/Garage/master/LightUpTheChristmas/lights.css'));
-	$("body").prepend('<div id="christmaslights" title="Te is szeretnél ilyet a blogodra / oldaladra? http://sandros.hu/?xmasbar"></div><audio id="christmaslightsaudio" src="https://github.com/sandrosdj/Garage/blob/master/LightUpTheChristmas/glass.ogg" preload></audio>');
-	for (var i = 0; i < (window.innerWidth / 64 - 1); i++)
-		$("#christmaslights").append("<div class='light color"+Math.floor(Math.random()*3)+"'></div>");
-	var lutca = document.getElementById('christmaslightsaudio');
-	$("#christmaslights .light").hover(function() {
-		if (!$(this).hasClass('broken') && !christmasblock)
-		{
-			$(this).addClass('broken');
-			lutca.volume = 0.15;
-			lutca.currentTime = 0;
-			lutca.play();
-			christmasblock = 1;
-			setTimeout('christmasblock = 0', 500);
-		}
-	}).click(function() {
-		$(this).parent().slideUp();
-	});
+$("head").append($('<link rel="stylesheet" type="text/css" />').attr('href', 'https://raw.githubusercontent.com/sandrosdj/Garage/master/LightUpTheChristmas/lights.css'));
+$("body").prepend('<div id="christmaslights" title="Te is szeretnél ilyet a blogodra / oldaladra? http://sandros.hu/?xmasbar"></div><audio id="christmaslightsaudio" src="https://github.com/sandrosdj/Garage/blob/master/LightUpTheChristmas/glass.ogg" preload></audio>');
+for (var i = 0; i < (window.innerWidth / 64 - 1); i++)
+	$("#christmaslights").append("<div class='light color"+Math.floor(Math.random()*3)+"'></div>");
+var lutca = document.getElementById('christmaslightsaudio');
+$("#christmaslights .light").hover(function() {
+	if (!$(this).hasClass('broken') && !christmasblock)
+	{
+		$(this).addClass('broken');
+		lutca.volume = 0.15;
+		lutca.currentTime = 0;
+		lutca.play();
+		christmasblock = 1;
+		setTimeout('christmasblock = 0', 500);
+	}
+}).click(function() {
+	$(this).parent().slideUp();
 });
