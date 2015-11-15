e5929fb5328689f45126b5facf15067cf8a9f232
Sandros <me@sandros.hu>
2015-11-15 20:30:48 +0100

    OTYOE Updata

--- LightUpTheChristmas/bulbs.jquery.js
+++ LightUpTheChristmas/bulbs.jquery.js
@@ -16,47 +16,4 @@ $("#christmaslights .light").hover(function() {
 }).click(function() {
 	$(this).parent().slideUp();
 });
-$("body").append('<style>#christmaslights {
-	position: fixed;
-	top: 0;
-	left: 0;
-
-	height: 64px;
-	width: 100%;
-
-	overflow: hidden;
-	cursor: pointer;
-
-	z-index: 12;
-}
-
-#christmaslights .light {
-	float: left;
-
-	width: 64px;
-	height: 64px;
-
-	background: url(https://raw.githubusercontent.com/sandrosdj/Garage/master/LightUpTheChristmas/bulbs.png);
-	background-position: 0px 0px;
-}
-
-#christmaslights .light.color0.broken {
-	background-position: -128px 0px;
-}
-#christmaslights .light.color0 {
-	background-position: 0px 0px;
-}
-
-#christmaslights .light.color1.broken {
-	background-position: -128px -64px;
-}
-#christmaslights .light.color1 {
-	background-position: 0px -64px;
-}
-
-#christmaslights .light.color2.broken {
-	background-position: -128px -128px;
-}
-#christmaslights .light.color2 {
-	background-position: 0px -128px;
-}</style>');
+$("body").append('<style>#christmaslights{position:fixed;top:0;left:0;height:64px;width:100%;overflow:hidden;cursor:pointer;z-index:12}#christmaslights .light{float:left;width:64px;height:64px;background:url(https://raw.githubusercontent.com/sandrosdj/Garage/master/LightUpTheChristmas/bulbs.png)}#christmaslights .light.color0.broken{background-position:-128px 0}#christmaslights .light.color0{background-position:0 0}#christmaslights .light.color1.broken{background-position:-128px -64px}#christmaslights .light.color1{background-position:0 -64px}#christmaslights .light.color2.broken{background-position:-128px -128px}#christmaslights .light.color2{background-position:0 -128px}</style>');
