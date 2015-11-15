7b28a0699dcc4b3cba7cdef2ff185bbda22e2682
Sandros <me@sandros.hu>
2015-11-15 20:29:40 +0100

    OTYOE Updata

--- LightUpTheChristmas/bulbs.jquery.js
+++ LightUpTheChristmas/bulbs.jquery.js
@@ -1,20 +1,62 @@
 var christmasblock = 0;
-$("head").append('<link rel="stylesheet" href="https://raw.githubusercontent.com/sandrosdj/Garage/master/LightUpTheChristmas/lights.css" type="text/css" media="screen" />');
-if (document.createStyleSheet) document.createStyleSheet('https://raw.githubusercontent.com/sandrosdj/Garage/master/LightUpTheChristmas/lights.css');
 $("body").prepend('<div id="christmaslights" title="Te is szeretnél ilyet a blogodra / oldaladra? http://sandros.hu/?xmasbar"></div><audio id="christmaslightsaudio" src="https://github.com/sandrosdj/Garage/blob/master/LightUpTheChristmas/glass.ogg" preload></audio>');
 for (var i = 0; i < (window.innerWidth / 64 - 1); i++)
 	$("#christmaslights").append("<div class='light color"+Math.floor(Math.random()*3)+"'></div>");
-//var lutca = document.getElementById('christmaslightsaudio');
+var lutca = document.getElementById('christmaslightsaudio');
 $("#christmaslights .light").hover(function() {
 	if (!$(this).hasClass('broken') && !christmasblock)
 	{
 		$(this).addClass('broken');
-//		lutca.volume = 0.15;
-//		lutca.currentTime = 0;
-//		lutca.play();
+		lutca.volume = 0.15;
+		lutca.currentTime = 0;
+		lutca.play();
 		christmasblock = 1;
 		setTimeout('christmasblock = 0', 500);
 	}
 }).click(function() {
 	$(this).parent().slideUp();
 });
+$("body").append('<style>#christmaslights {
+	position: fixed;
+	top: 0;
+	left: 0;
+
+	height: 64px;
+	width: 100%;
+
+	overflow: hidden;
+	cursor: pointer;
+
+	z-index: 12;
+}
+
+#christmaslights .light {
+	float: left;
+
+	width: 64px;
+	height: 64px;
+
+	background: url(https://raw.githubusercontent.com/sandrosdj/Garage/master/LightUpTheChristmas/bulbs.png);
+	background-position: 0px 0px;
+}
+
+#christmaslights .light.color0.broken {
+	background-position: -128px 0px;
+}
+#christmaslights .light.color0 {
+	background-position: 0px 0px;
+}
+
+#christmaslights .light.color1.broken {
+	background-position: -128px -64px;
+}
+#christmaslights .light.color1 {
+	background-position: 0px -64px;
+}
+
+#christmaslights .light.color2.broken {
+	background-position: -128px -128px;
+}
+#christmaslights .light.color2 {
+	background-position: 0px -128px;
+}</style>');
