028316f5b33dae78bf2b1e2f1a16a2871252dc4f
Sandros <me@sandros.hu>
2015-11-15 20:24:18 +0100

    OTYOE Updata

--- LightUpTheChristmas/bulbs.jquery.js
+++ LightUpTheChristmas/bulbs.jquery.js
@@ -4,14 +4,14 @@ if (document.createStyleSheet) document.createStyleSheet('https://raw.githubuser
 $("body").prepend('<div id="christmaslights" title="Te is szeretnél ilyet a blogodra / oldaladra? http://sandros.hu/?xmasbar"></div><audio id="christmaslightsaudio" src="https://github.com/sandrosdj/Garage/blob/master/LightUpTheChristmas/glass.ogg" preload></audio>');
 for (var i = 0; i < (window.innerWidth / 64 - 1); i++)
 	$("#christmaslights").append("<div class='light color"+Math.floor(Math.random()*3)+"'></div>");
-var lutca = document.getElementById('christmaslightsaudio');
+//var lutca = document.getElementById('christmaslightsaudio');
 $("#christmaslights .light").hover(function() {
 	if (!$(this).hasClass('broken') && !christmasblock)
 	{
 		$(this).addClass('broken');
-		lutca.volume = 0.15;
-		lutca.currentTime = 0;
-		lutca.play();
+//		lutca.volume = 0.15;
+//		lutca.currentTime = 0;
+//		lutca.play();
 		christmasblock = 1;
 		setTimeout('christmasblock = 0', 500);
 	}
