ef3bd01d7a49172de621849d843de870b979dbaa
Sandros <me@sandros.hu>
2015-11-15 20:08:34 +0100

    OTYOE Updata

--- LightUpTheChristmas/bulbs.jquery.js
+++ LightUpTheChristmas/bulbs.jquery.js
@@ -1,17 +1,16 @@
 var christmasblock = 0;
 $(document).ready(function() {
-		$("body").prepend('<div id="christmaslights" title="Te is szeretnél ilyet a blogodra / oldaladra? http://sandros.hu/?xmasbar"></div><audio id="christmaslightsaudio" src="http://thinkdot.tk/images/events/glass1.ogg" preload></audio>');
-
+		$("body").prepend('<div id="christmaslights" title="Te is szeretnél ilyet a blogodra / oldaladra? http://sandros.hu/?xmasbar"></div><audio id="christmaslightsaudio" src="//github.com/sandrosdj/Garage/blob/master/LightUpTheChristmas/glass.ogg" preload></audio>');
 		for (var i = 0; i < (window.innerWidth / 64 - 1); i++)
 			$("#christmaslights").append("<div class='light color"+Math.floor(Math.random()*3)+"'></div>");
-		
+		var lutca = document.getElementById('christmaslightsaudio');
 		$("#christmaslights .light").hover(function() {
 			if (!$(this).hasClass('broken') && !christmasblock)
 			{
 				$(this).addClass('broken');
-				document.getElementById('christmaslightsaudio').volume = 0.15;
-				document.getElementById('christmaslightsaudio').currentTime = 0;
-				document.getElementById('christmaslightsaudio').play();
+				lutca.volume = 0.15;
+				lutca.currentTime = 0;
+				lutca.play();
 				christmasblock = 1;
 				setTimeout('christmasblock = 0', 500);
 			}
